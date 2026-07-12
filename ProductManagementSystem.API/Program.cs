3813cadf04a3c3b14b0d67dff8502b9ea12721ee
pongkorn <pongkorn.l169@gmail.com>
2026-07-12 12:23:23 +0700

    chore(swagger): configure launchBrowser for swagger

--- ProductManagementSystem.API/Program.cs
+++ ProductManagementSystem.API/Program.cs
@@ -70,9 +70,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+// เปิดใช้งาน Swagger เสมอสำหรับขั้นตอนการพัฒนาและเทสระบบ
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
